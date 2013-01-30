3a0b45da4a0802b89cf30776b45c9ad38862f45f
Harald Lapp <harald.lapp@gmail.com>
2013-01-30 16:58:28 +0100

    merge styles with javascripts

--- bin/build.sh
+++ bin/build.sh
@@ -7,6 +7,7 @@ if [[ ! -d $ROOT/web ]]; then
 	mkdir $ROOT/web
 fi
 
+# combine javascripts
 if [[ "$JSMIN" != "" ]]; then
 	jsmin <$ROOT/src/hook.js | cat - \
 		$ROOT/src/vendor/jquery/jquery.js \
@@ -18,3 +19,8 @@ else
 		$ROOT/src/vendor/bpopup/jquery.bpopup-0.8.0.min.js \
 		$ROOT/src/vendor/highlight/highlight.pack.js > $ROOT/web/hook.pack.js
 fi
+
+# add styles
+echo "\$(document).ready(function() { \$('<style type=\"text/css\">" >> $ROOT/web/hook.pack.js
+tr -d '\n' < $ROOT/src/vendor/highlight/solarized_light.css >> $ROOT/web/hook.pack.js 
+echo "</style>').appendTo(\$('head')});" >> $ROOT/web/hook.pack.js
