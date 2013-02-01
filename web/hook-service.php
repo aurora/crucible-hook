dbb92571996c76c9b84b1be6c31fc81c8f3bd912
Harald Lapp <harald.lapp@gmail.com>
2013-02-01 15:01:21 +0100

    added .git extension

--- web/hook-service.php
+++ web/hook-service.php
@@ -19,13 +19,13 @@ $c = (isset($_GET['c']) && preg_match('/^[a-f0-9]{7}$/', $_GET['c'])
         ? $_GET['c']
         : false);
 
-if (!$r || !$b || !$c || !is_dir(GIT_REPOS . '/' . $r)) {
+if (!$r || !$b || !$c || !is_dir(GIT_REPOS . '/' . $r . '.git')) {
     die('invalid "r", "b" or "c"');
 }
 
 switch ($_GET['a']) {
 case 'diff':
-    chdir(GIT_REPOS . '/' . $r);
+    chdir(GIT_REPOS . '/' . $r . '.git');
     passthru(sprintf(
         'git diff %s',
         escapeshellarg($c . '^!')
