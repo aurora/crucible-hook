b9dd883874d1fd0e5fbf3dba8f5dcc1cb108f29e
Harald Lapp <harald.lapp@gmail.com>
2013-02-01 15:00:32 +0100

    don't include css in js pack, remoded bpopup

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,6 @@ ROOT=$(cd "$(dirname "$0")/.." && pwd)
 DEST=$ROOT/web/hook.pack.js
 DEPS=(
     $ROOT/src/vendor/jquery/jquery.js
-    $ROOT/src/vendor/bpopup/jquery.bpopup-0.8.0.min.js
     $ROOT/src/vendor/highlight/highlight.pack.js
 )
 
@@ -24,8 +23,3 @@ else
 fi
 
 echo "" >> $DEST
-
-# add styles
-echo -n "jQuery(document).ready(function(\$) { \$('<style type=\"text/css\">" >> $DEST
-tr -d '\n' < $ROOT/src/vendor/solarized/solarized_light.css >> $DEST
-echo "</style>').appendTo(\$('head')); });" >> $DEST
